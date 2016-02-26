588fcbf0e34d0aff41636cb78c3d47b8de732640
yjtx <friendhhy@163.com>
2016-02-26 10:36:08 +0800

    增加2种特殊类型

--- api/test/classes/types/AllTypes.ts
+++ api/test/classes/types/AllTypes.ts
@@ -15,18 +15,43 @@ module yjtx {
 
         c2:(p1:number, p2:string)=>A|boolean;
 
+        c3:(p1:(p1:number|A, p2:string)=>number|A, p2:string)=>A|boolean;
 
-        skins: { [component: string]: string };
+
+        skins1: { [component: string]: string };
+        skins2: { component: string };
 
         constructor(type:string | A, bubbles?:boolean|string, cancelable?:boolean) {
 
         }
 
-        public static create<T extends A>(EventClass:{new (type:string | A, bubbles?:boolean|string, cancelable?:boolean): T; eventPool?:A[]},
+        public static create<T>(EventClass:{new (type:string | A, bubbles?:boolean|string, cancelable?:boolean): T; eventPool?:A[]},
                                               type:string, bubbles?:boolean, cancelable?:boolean):T {
 
             return new EventClass(type, bubbles, cancelable);
         }
     }
 
+
+    export interface BBB {
+        name:string;
+        isFile:boolean;
+
+        (source: string, subString: string): boolean;
+    }
+
+    export interface AAA {
+        [key:string]:BBB;
+
+        d:{[key:string]:AAA};
+
+        a:boolean;
+    }
+
+
+
+    //var a:AAA;
+    //var b = a["test"].isFile;
 }
+
+
