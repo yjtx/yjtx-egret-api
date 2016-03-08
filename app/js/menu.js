c9e7002822d05ad571b5eee21f83b170d42cf6ad
yjtx <friendhhy@163.com>
2016-03-08 20:37:53 +0800

    修改

--- app/js/menu.js
+++ app/js/menu.js
@@ -1,135 +1,113 @@
-var remote = require('remote');
-var Menu = remote.require('menu');
 
 var template = [
     {
-        label: 'File',
+        label: '编辑',
         submenu: [
             {
-                label: 'back',
-                accelerator: 'CmdOrCtrl+Z',
-                role: 'undo'
-            }
-        ]
-    },
-    {
-        label: 'File',
-        submenu: [
-            {
-                label: '配置',
-                click: showConfig
+                label: '隐藏',
+                accelerator: 'Cmd+H',
+                role: 'hide'
             },
             {
-                label: 'api查看',
-                accelerator: 'CmdOrCtrl+Z',
-                click: showApi
-            }
-        ]
-    },
-    {
-        label: 'Edit',
-        submenu: [
-            {
-                label: 'Undo',
-                accelerator: 'CmdOrCtrl+Z',
-                role: 'undo'
-            },
-            {
-                label: 'Redo',
-                accelerator: 'Shift+CmdOrCtrl+Z',
-                role: 'redo'
+                label: '隐藏其他',
+                accelerator: 'Shift+Cmd+H',
+                role: 'hide'
             },
             {
                 type: 'separator'
             },
             {
-                label: 'Cut',
-                accelerator: 'CmdOrCtrl+X',
-                role: 'cut'
-            },
-            {
-                label: 'Copy',
-                accelerator: 'CmdOrCtrl+C',
-                role: 'copy'
-            },
-            {
-                label: 'Paste',
-                accelerator: 'CmdOrCtrl+V',
-                role: 'paste'
-            },
-            {
-                label: 'Select All',
-                accelerator: 'CmdOrCtrl+A',
-                role: 'selectall'
-            },
+                label: '退出',
+                click: function() { app.quit(); },
+                accelerator: (function() {
+                    if (process.platform == 'darwin')
+                        return 'Command+Q';
+                    else
+                        return 'ALT+F4';
+                })()
+            }
         ]
     },
     {
-        label: 'View',
+        label: '编辑',
         submenu: [
             {
-                label: 'Reload',
-                accelerator: 'CmdOrCtrl+R',
-                click: function(item, focusedWindow) {
-                    if (focusedWindow)
-                        focusedWindow.reload();
-                }
-            },
-            {
-                label: 'Toggle Full Screen',
+                label: '配置',
+                click: showConfig,
                 accelerator: (function() {
                     if (process.platform == 'darwin')
-                        return 'Ctrl+Command+F';
+                        return 'Command+F';
                     else
                         return 'F11';
-                })(),
-                click: function(item, focusedWindow) {
-                    if (focusedWindow)
-                        focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
-                }
-            },
-            {
-                label: 'Toggle Developer Tools',
-                accelerator: (function() {
-                    if (process.platform == 'darwin')
-                        return 'Alt+Command+I';
-                    else
-                        return 'Ctrl+Shift+I';
-                })(),
-                click: function(item, focusedWindow) {
-                    if (focusedWindow)
-                        focusedWindow.toggleDevTools();
-                }
-            },
+                })()
+            }
         ]
     },
     {
-        label: 'Window',
+        label: '视图',
+        submenu: [
+            {
+                label: 'api查看',
+                click: showApi,
+                accelerator: (function() {
+                    if (process.platform == 'darwin')
+                        return 'Command+R';
+                    else
+                        return 'F12';
+                })()
+            }
+        ]
+    },
+    {
+        label: '窗口',
         role: 'window',
         submenu: [
             {
-                label: 'Minimize',
-                accelerator: 'CmdOrCtrl+M',
+                label: '最小化',
+                accelerator: 'Cmd+M',
                 role: 'minimize'
             },
             {
-                label: 'Close',
-                accelerator: 'CmdOrCtrl+W',
+                label: '关闭',
+                accelerator: 'Cmd+W',
                 role: 'close'
             },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Bring All to Front',
+                role: 'front'
+            }
         ]
     },
     {
-        label: 'Help',
+        label: '帮助',
         role: 'help',
         submenu: [
             {
-                label: 'Learn More',
-                click: function() { require('electron').shell.openExternal('http://electron.atom.io') }
+                label: 'source',
+                click: function() { shell.openExternal('https://github.com/yjtx/yjtx-egret-api') }
             },
+            {
+                label: '关于',
+                click: function() { shell.openExternal('http://electron.atom.io') }
+            },
+            {
+                label: 'About ',
+                role: 'about'
+            }
         ]
     }
 ];
+
+const electron = require('electron');
+const shell    = electron.shell;
+
+const remote     = electron.remote;
+var Menu = remote.require('menu');
+var app = remote.require('app');
+
 
 
 var menu = Menu.buildFromTemplate(template);
